e5e2a688bf5802b8314402cbad322a98696c1cec
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-06-28 11:30:17 +0530

    good working after deploy

---
+++ .history/src/constants/Constant_20240627200035.jsx
@@ -0,0 +1,22 @@
+import Python from "../assets/Python.png";
+import Javascript from "../assets/Javascript.png";
+import Git from "../assets/Git.png";
+import Vite from "../assets/Vite.png";
+import TailwindCSS from "../assets/TailwindCSS.png";
+
+import Vue from "../assets/vue.png";
+import nodejs from "../assets/nodejs.png";
+import Github from "../assets/Github.png";
+import Portfolio from "../assets/portfolio.jpg";
+
+export {
+  Github,
+  Git,
+  Vite,
+  TailwindCSS,
+  Python,
+  Javascript,
+  Vue,
+  Portfolio,
+  nodejs,
+};
---
+++ .history/src/constants/Constant_20240627200212.jsx
@@ -0,0 +1,25 @@
+import HTML from "../assets/HTML.png";
+import Github from "../assets/Github.png";
+import CSS from "../assets/CSS.png";
+import Python from "../assets/Python.png";
+import Javascript from "../assets/Javascript.png";
+import Git from "../assets/Git.png";
+import Vite from "../assets/Vite.png";
+import TailwindCSS from "../assets/TailwindCSS.png";
+import react from "../assets/react.png";
+
+import nodejs from "../assets/nodejs.png";
+
+export {
+  HTML,
+  CSS,
+  Github,
+  Git,
+  Vite,
+  TailwindCSS,
+  react,
+  Python,
+  Javascript,
+  Portfolio,
+  nodejs,
+};
