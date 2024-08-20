c6630ccac959d6d9a029b6aaf9bca5d921a4f7f4
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-08-20 21:14:37 +0530

    added view button

---
+++ .history/src/components/cards_props_20240820200340.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+
+const Project_prop = (props) => {
+  return (
+    <>
+      <div className="card bg-sky-50 dark:bg-blue-950 rounded-lg shadow-lg   ">
+        <h3 className="title capitalize font-bold h-20 text-slate-800 dark:text-slate-300 text-xl max-sm:text-lg m-5 text-center">
+          {props.title}
+        </h3>
+        <div className="bar h-1 dark:bg-gray-400  bg-slate-600  mx-5"></div>
+        <div className="flex gap-1 ml-3 mt-3 ">
+          {props.react}
+          {props.vite}
+          {props.html5}
+          {props.css3}
+          {props.php}
+          {props.mysql}
+          {props.javascript}
+          {props.tailwindcss}
+          {props.laravel}
+          {props.bootstrap}
+          {props.wordpress}
+        </div>
+        <p className="dark:text-slate-500  text-slate-900 mt-2 text-sm px-5   select-none">
+          {props.para}
+        </p>
+
+        <div className="absolute bottom-10 left-1/2 translate-x-1/2 translate-y-1/2 ">
+          <a className=" " href={props.github_link}>
+            <button className=" items-center text-sm font-medium text-lime-300 mx-1 bg-transparent border border-lime-600 py-3 px-5 rounded-xl ">
+              View
+            </button>
+          </a>
+          <a className=" " href={props.github_link}>
+            <button className="text-sm font-medium text-lime-300 mx-1 bg-transparent border border-lime-600 py-3 px-5 rounded-xl ">
+              Code
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Project_prop;
--- .history/src/components/proj_20240613132039.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const proj = () => {
-  return <div>proj</div>;
-};
-
-export default proj;
